92a78778137866cd67dfaf8af634b76cc22d1582
t0in4 <t0in4@yahoo.com>
2023-01-30 19:25:36 +0900

    19:25

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,12 @@ import referencing.Reference;
 import referencingParent.ReferencingParent;
 
 public class Main {
+    // мы используем каст (изменение) типа класса
+    public static void printNames(Person[] persons) {
+        for (Person person : persons) {
+            System.out.println(person.getName());
+        }
+    }
     public static void main(String[] args) {
         Reference a1 = new Reference();
 
@@ -31,9 +37,27 @@ public class Main {
         //employee2.setSalary(30000); // родительский класс не знает о методе setSalary()
         // но у него есть методы setName() setYearOfBirth()
 
-        
+        // Назначение типа класса
+        // 1. обработка массива объектов у которых разные типы из одной иерархии
+        // тип Person, тип Employee, тип Client
+        // 2. метод который есть у объекта в базовом (родительском) классе, но также может
+        // работать с объектами подкласса например: .setName();
+        Person person2 = new Client();
+        //Employee employee3 = (Employee) person2; // здесь будет ошибка ClassCastException
+        // class example.Client cannot be cast to class example.Employee (example.Client and example.Employee are in unnamed module of loader 'app')
 
 
+        //Client clientAgain = (Client) person; // здесь не будет ошибок
+
+        Person person3 = new Employee();
+        person3.setName("Ann");
+        Client client3 = new Client();
+        client3.setName("Pauline");
+        Employee employee4 = new Employee();
+        employee4.setName("Alice");
+
+        Person[] persons = {person3, client3, employee4, employee2};
+        printNames(persons);
 
 
 
